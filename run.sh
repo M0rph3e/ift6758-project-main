29c160a4e35608c59fca8f35b1bd1928dc10938b
unknown <yassmams16@gmail.com>
2021-12-19 01:19:20 -0500

    Docker Part 3 done but my brain is dead so pls no more today

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 echo "Running..."
 
-docker run -it --expose 127.0.0.1:5000:5000/tcp --env DOCKER_ENV_VAR=$COMET_API_KEY ift6758/serving:1.0.0
-echo $COMET_API_KEY
+docker run -it -e 127.0.0.1:5000:5000/tcp --env COMET_API_KEY=$COMET_API_KEY ift6758/serving:$1
+#echo $COMET_API_KEY
