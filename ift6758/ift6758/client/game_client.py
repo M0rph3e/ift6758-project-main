40d5b9868540023a368eeb6fbb39524f8dcd934e
Sai Kalyan Siddanatham <saikalyan@cse.iitb.ac.in>
2021-12-20 05:46:42 -0500

    Corrected the preprocessing to make predictions consistent overt time

--- ift6758/ift6758/client/game_client.py
+++ ift6758/ift6758/client/game_client.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 import logging
 from ift6758.features.feature_engineering_game import GameData
-
+import numpy as np
 logger = logging.getLogger(__name__)
 
 
@@ -20,6 +20,19 @@ class GameClient:
         game_data = GameData(gameId=game_id)
         df_game,last_event = game_data.get_features_2()
         df_game_filtered = df_game.loc[df_game["eventId"]>=non_processed_event]
+        
+
         categories = ['shotType','gamePeriod','lastEventType','rebound','isHome']
-        df_game_filtered[categories]=df_game_filtered[categories].astype("category")
+        shotType_categories = ['Backhand', 'Deflected', 'Slap Shot', 'Snap Shot', 'Tip-In', 'Wrap-around', 'Wrist Shot']
+        gamePeriod_categories  = [1,2,3,4,5]
+        lastEventType_categories = ['Blocked Shot', 'Faceoff', 'Game Official', 'Giveaway', 'Goal', 'Hit','Missed Shot', 'Official Challenge', 'Penalty', 'Period End','Period Official', 'Period Ready', 'Period Start', 'Shootout Complete','Shot', 'Stoppage', 'Takeaway']
+        rebound_categories= [False, True]
+        isHome_categories = [False, True]
+        categories_dict = dict(zip(['shotType','gamePeriod','lastEventType','rebound','isHome'],[shotType_categories,gamePeriod_categories,lastEventType_categories,rebound_categories,isHome_categories]))
+        for category in categories:
+            df_game_filtered[category] = pd.Categorical(df_game_filtered[category],categories =categories_dict[category] )
+        # df_game_filtered[categories]=df_game_filtered[categories].astype("category")
+        df_game_filtered.replace([np.inf, -np.inf], np.nan, inplace=True)
+        df_game_filtered.replace([np.inf, -np.inf], np.nan, inplace=True)
+        df_game_filtered["changeInShotAngle"].replace(0,np.nan,inplace=True)
         return df_game_filtered,last_event
